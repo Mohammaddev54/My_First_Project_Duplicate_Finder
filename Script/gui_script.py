fe0f59d99893d92abf6a4b9fd6fefbd358a57a80
Mohammaddev54 <mohammaddev54@gmail.com>
2025-03-14 20:58:30 +0430

    Update!
    - Some changes made!

--- Script/gui_script.py
+++ Script/gui_script.py
@@ -4,18 +4,23 @@ GUI setup script for duplicate Searching application.
 
 from tkinter import ttk, Tk, Label, Entry, Button
 
-class Duplicate_Finder_GUI():
+class win():
     def __init__(self, root=None):
         
         if root == None:
             root = Tk()
+            # root.grab_set()
             self.root = root
         else:
             self.root = root
         
+        
         self.root.geometry("500x320")
         self.root.title("Search Duplicates")
         self.root.resizable(False, False)
+        
+        self.root.bind("<Escape>", lambda event: self.root.destroy())
+        self.root.bind("<Return>", lambda event: self.search_button_action())
         
         self.top_frame         = ttk.Frame(self.root, padding = (10), borderwidth = 2, relief="solid")
         self.bottom_frame      = ttk.Frame(self.root, padding = (10), borderwidth = 2, relief="solid")
@@ -33,8 +38,8 @@ class Duplicate_Finder_GUI():
         self.destination_entry.grid(row = 1, column = 1, ipadx= 5, ipady = 5)
         
         self.search_button      =    Button(self.top_frame, text="Search", pady = 5, padx = 5, font= (14), command = self.search_button_action)
-        self.undo_button        =     Button(self.bottom_frame, text="Undo", pady = 5, padx = 5, font= (14), command = self.undo_button_action)
-        self.cancel_button      = Button(self.bottom_frame, text="Cancel", pady = 5, padx = 5, font= (14), command = self.cancel_button_action)
+        self.undo_button        =     Button(self.bottom_frame, text="Undo", pady = 5, padx = 5, font= (14), state="disabled", command = self.undo_button_action)
+        self.cancel_button      = Button(self.bottom_frame, text="Cancel", pady = 5, padx = 5, font= (14),state="disabled", command = self.cancel_button_action)
         
         self.search_button.grid(           row = 2, column = 0)
         self.undo_button.grid(  row = 0, column = 0, padx = 10)
@@ -43,6 +48,10 @@ class Duplicate_Finder_GUI():
         self.top_frame.pack(padx = 10, pady = 10, side = "top", fil  = 'x')
         self.bottom_frame.pack(           padx = 10, pady = 10, fill = 'x')
         
+    # Missing code Function
+    def not_implemented(self, location):
+        print(f"{location}: Code Missing!")
+
     def search_button_action(self):
         pass
     
@@ -55,6 +64,6 @@ class Duplicate_Finder_GUI():
         pass
 
 if __name__ == "__main__":
-    print("RUNNING GUI SCRIPT in terminal")
-    window = Duplicate_Finder_GUI()
+    print("gui running in terminal")
+    window = win()
     window.root.mainloop()
